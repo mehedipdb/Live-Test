f5f009824d300f91e4819da57060bf26c9f821c5
Mehedi <mehedipdb@gmail.com>
2024-06-24 23:07:40 +0600

    This is Live Test File

--- main.dart
+++ main.dart
@@ -1,3 +1,33 @@
-void main(){
-  print('mehedi');
+// Main function
+void main() {
+  // Creating an instance of Media
+  Media media = Media();
+  media.play(); // Calling play() method of Media class
+
+  // Creating an instance of Song
+  Song song = Song('Artist Name');
+  song.play(); // Calling play() method of Song class
 }
+// Base class Media
+class Media {
+  // Method play() in Media class
+  void play() {
+    print('Playing media...');
+  }
+}
+
+// Derived class Song that inherits from Media
+class Song extends Media {
+  // Additional attribute artist
+  String artist;
+
+  // Constructor for Song class
+  Song(this.artist);
+
+  // Overriding the play() method
+  @override
+  void play() {
+    print('Playing song by $artist...');
+  }
+}
+
